fd3f2fac4c101a6ca2d104688efcfd011490d984
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-06-17 15:35:26 +1000

    added a home page route and did a little bit to make it pretty"

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -69,6 +69,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:user_id, :avatar, :language, :age, :location, :number_of_students, :gender, :bio)
+      params.permit(:user_id, :avatar, :language, :age, :location, :number_of_students, :gender, :bio)
     end
 end
